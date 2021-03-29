1337c12a5d3b5763f27c543c4d55d92421a55f66
Adithyan K <adi.k.dev@gmail.com>
2021-03-29 22:45:21 +0530

    new main class

---
+++ java-app/src/main/java/com/indtele/videohelper/AMain.java
@@ -0,0 +1,32 @@
+package com.indtele.videohelper;
+
+import java.util.Arrays;
+
+public class AMain
+{
+    public static void main(String[] args) throws Exception
+    {
+        System.out.println("Main Entry!");
+        System.out.println("args = " + Arrays.toString(args));
+
+        if (args.length == 0)
+        {
+            System.out.println("args not given. Exiting...");
+            System.exit(0);
+        }
+
+        String cmd = args[0];
+
+        if (cmd.equals("downloadSourceData"))
+        {
+            PDFCreator c = new PDFCreator();
+            c.downloadAll();
+        }
+        else if (cmd.equals("createTextFiles"))
+        {
+            PDFCreator c = new PDFCreator();
+            c.prepareVideoHelperDoc();
+        }
+    }
+}
+
